3576da840fc9b8adaba62be2a71ddfcedd9130f5
manipvlator <mr.morat14@gmail.com>
2026-06-27 14:54:54 +0700

    kernel: apk_sign: add more kernelsu fork support
    added support for
    - KernelSU-Next
    - MamboSU
    - ReSukiSU
    
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -360,5 +360,17 @@ bool is_manager_apk(char *path)
 	if (check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"))
 		return true;
 
+	// rifsxd/KernelSU-Next
+	if (check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"))
+		return true;
+
+	// RapliVx/KernelSU
+	if (check_v2_signature(path, 0x384, "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"))
+		return true;
+
+	// ReSukiSU/ReSukiSU
+    if (check_v2_signature(path, 0x377, "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"))
+        return true;
+
 	return false;
 }
